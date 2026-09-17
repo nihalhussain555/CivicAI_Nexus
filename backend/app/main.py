23428f9ff47a75d6df95c7382df0f7c4139192f8
Nihal Hussain <kit28.24bcs106@gmail.com>
2026-09-17 23:05:34 +0530

    Add new origin to CORS middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -71,7 +71,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.FRONTEND_URL, "http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[settings.FRONTEND_URL, "http://localhost:5173", "http://127.0.0.1:5173","https://civicai-theta.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
